3a2f35d529f2f33d6471b5548576671eb49afe48
marcosradix <marcosradix@gmail.com>
2018-03-29 18:40:51 -0300

    projeto my-money fazendo backend

---
+++ backend/src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+module.exports = mongoose.connect('mongodb://localhost/mymoney');
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = "O valor '{VALUE}' informado é menor que o limite de {MIN}"
+mongoose.Error.messages.Number.max = "O valor '{VALUE}' informado é maior que o limite máximo de  {MAX}"
+mongoose.Error.messages.String.enum = "O '{VALUE}' informado não é válido para o atributo {PATH}"
