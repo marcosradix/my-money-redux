33037a4ea2a56044e6637167e153af7828754417
MARCOS FERREIRA SEVERINO <006427L3@sefaz-ce.gov.br>
2018-04-02 17:33:31 -0300

    listando do banco de dados na lista compo ações

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -30,6 +30,7 @@ class BillingCycleList extends Component{
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
+                            <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
