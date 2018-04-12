aee8ad8ad350e2d97fbc04ba66a263b7cadf110f
MARCOS FERREIRA SEVERINO <006427L3@sefaz-ce.gov.br>
2018-04-12 13:06:49 -0300

    projeto fizalizado backend e frontend

--- frontend/src/main/routers.jsx
+++ frontend/src/main/routers.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {Router, Route, IndexRoute, Redirect, hashHistory} from 'react-router';
 import DashBoard from '../dashboard/dashboard';
 import BillingCycle from '../billingCycle/billingCycle';
-import App from '../App';
+import AuthOrApp from '../authOrApp';
 
 export default props =>(
 <Router history={hashHistory} >
-    <Route path='/' component={App} >
+    <Route path='/' component={AuthOrApp} >
         <IndexRoute component={DashBoard} />
         <Route path='billingCycles' component={BillingCycle} />
     </Route>
