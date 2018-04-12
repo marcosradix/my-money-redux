f64e3ded179532050a32ffefae01c451b5e8e9be
MARCOS FERREIRA SEVERINO <006427L3@sefaz-ce.gov.br>
2018-04-12 13:52:57 -0300

    atualizações no frontend

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -1,7 +1,7 @@
 
 
-const IN_API_URL = 'http://localhost:3003/api';
-const IN_OAPI_URL = 'http://localhost:3003/oapi';
+const IN_API_URL = 'http://localhost:3003/api'; //https://nomedoapp.herokuapp.com/api
+const IN_OAPI_URL = 'http://localhost:3003/oapi'; ////https://nomedoapp.herokuapp.com/oapi
 export default {
     API_URL: IN_API_URL,
     OAPI_URL: IN_OAPI_URL,
