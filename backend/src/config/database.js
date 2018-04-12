9183c5ec8f1003268e5d18b8ba5b0dd22c30bb8d
MARCOS FERREIRA SEVERINO <006427L3@sefaz-ce.gov.br>
2018-04-12 13:26:41 -0300

    atualização da url do backend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongodb://localhost/mymoney');
 
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney';
+module.exports = mongoose.connect(url);
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
 mongoose.Error.messages.Number.min = "O valor '{VALUE}' informado é menor que o limite de {MIN}"
 mongoose.Error.messages.Number.max = "O valor '{VALUE}' informado é maior que o limite máximo de  {MAX}"
